98a782d78015f360c3c306a746488cad583b750c
Satria Irawan <satriai418@gmail.com>
2024-11-03 15:32:39 +0700

    Change time to local id

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        \Illuminate\Database\Eloquent\Model::unguard();
     }
 
     /**
@@ -19,6 +19,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Locale
+        setlocale(LC_TIME, 'id_ID');
+        \Carbon\Carbon::setLocale('id');
+        \Illuminate\Support\Facades\App::setLocale('id');
+
+        // Pagination
+        \Illuminate\Pagination\Paginator::useBootstrapFive();
     }
 }
